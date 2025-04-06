0f6035573fcf08c5b1d36710f94a8e5756b1df68
Timo <timo.lampert@rwu.de>
2025-04-06 23:11:59 +0200

    Cleaned up

--- ldrag/tests/test_ontology.py
+++ ldrag/tests/test_ontology.py
@@ -1,5 +1,3 @@
-#Test Unit Case
-
 import unittest
 from ldrag.ontology import Ontology, GenericNode
 from ldrag.ontology import Node
@@ -9,25 +7,27 @@ class TestOntology(unittest.TestCase):
         self.ontology = Ontology()
         self.ontology.deserialize("../../gpt_chatbot_ldrag/ontology/ontology.json")  # Adjust the path to your ontology file
         self.node2 = Node("Test2", "Model", connections=[])
+        self.shap_length_node = self.ontology.get_node("SHAP_nn_2025_03_alx_num__length")
 
 
     def test_add_node(self):
-        #add more nodes
         self.node = Node("Test", "Model", connections=[self.node2])
         self.ontology.add_node(self.node)
         self.assertIs(self.node, self.ontology.get_node("Test"), "Node should be added to the ontology.")
 
     def test_get_connected_nodes(self):
-        # Test the connections of a node
-        self.node = self.ontology.get_node("SHAP_nn_2025_03_alx_num__length")
-        self.assertIn(self.ontology.get_node("nn_2025_03_alx"), self.ontology.get_connected_nodes(self.node).values(), "Connected nodes should include the connected node.")
+        self.assertIn(self.ontology.get_node("nn_2025_03_alx"), self.ontology.get_connected_nodes(self.shap_length_node).values(), "Connected nodes should include the connected node.")
 
 
     def test_get_node_structure(self):
-        # Test the structure of a node
-        self.node = self.ontology.get_node("SHAP_nn_2025_03_alx_num__length")
-        self.node_structure = self.ontology.get_node_structure(self.node)
+        self.node_structure = self.ontology.get_node_structure(self.shap_length_node)
         self.assertIsInstance(self.node_structure, dict, "Node structure should be a dictionary.")
         self.assertNotIn("connections", self.node_structure, "Node structure should not include connections.")
         self.assertIn("Connected Instances", self.node_structure, "Node structure should 'include Connected Instances'.")
-        print(self.ontology.get_ontology_structure())
+        self.assertGreater(len(self.node_structure), 0, "Node structure should not be empty.")
+
+    def test_get_ontology_structure(self):
+        ontology_structure = self.ontology.get_ontology_structure()
+        self.assertIsInstance(ontology_structure, list, "Ontology structure should be a dictionary.")
+        self.assertGreater(len(ontology_structure), 0, "Ontology structure should not be empty.")
+        self.assertIn("Node Class ID", ontology_structure[0].keys(), "Ontology structure should include 'node class id'.")
