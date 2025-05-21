900295807d470d05dc1e3702a38d25744496a68c
Timo <timo.lampert@rwu.de>
2025-05-21 21:55:25 +0200

    Working integration test

--- ldrag/tests/test_integration_retriever.py
+++ ldrag/tests/test_integration_retriever.py
@@ -1,8 +1,7 @@
+import json
 import ldrag.retriever as retriever
 import unittest
 import ldrag.ontology as ontology
-
-
 
 class IntegrationTestRetriever(unittest.TestCase):
 
@@ -10,13 +9,16 @@ class IntegrationTestRetriever(unittest.TestCase):
         self.ontology = ontology.Ontology()
         self.ontology.deserialize("./mocks/ontology.json")
 
-#TODO: implement
+    #TODO: tests for retriever with parameter
     def test_retrieve_information(self):
         retrieved_info,node_graph=retriever.information_retriever_with_graph(
             ontology=self.ontology,
-            user_query="Test",
+            user_query="Zylinder Schraube",
             previous_conversation=[]
         )
-        expected_output = 'SHAP_nn_2025_03_alx_num__length'
-        print("Retrieved information:", retrieved_info)
-        self.assertEqual(retrieved_info[0].get('Node Instance ID'), expected_output)
+        #print(json.dumps(retrieved_info, indent=4))
+
+        expected = "cat__screwType_Zylinder"
+        self.assertIn("screwType",json.dumps(retrieved_info),"expected ScrewType to be in retrieved info.")
+        self.assertTrue(node_graph , "Node graph should not be empty.")
+        self.assertIn(expected, json.dumps(retrieved_info), f"Expected '{expected}' to be in retrieved info.")
